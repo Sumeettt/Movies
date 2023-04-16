3a6fccf971f2238134c75b2649b6e27049461bc7
Sumeettt <sumeetp987@gmail.com>
2023-04-16 08:20:46 +0000

    api 4 not working

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const initializeDBAndServer = async () => {
 };
 initializeDBAndServer();
 
-//API 1
+//API 1 GET
 app.get("/movies/", async (request, response) => {
   const getlistOfMoviesQuery = ` SELECT movie_name FROM movie; `;
   const movieList = await db.all(getlistOfMoviesQuery);
@@ -42,4 +42,58 @@ app.get("/movies/", async (request, response) => {
   }
 
   response.send(movieArr);
+});
+
+//API 2 POST
+app.post("/movies/", async (request, response) => {
+  const requestBody = request.body;
+  const { directorId, movieName, leadActor } = requestBody;
+  const postMovieQuery = `
+        INSERT INTO 
+            movie (director_id,movie_name,lead_actor)
+        VALUES 
+        (
+            ${directorId},
+            '${movieName}',
+            '${leadActor}'
+        );`;
+  const dbResponse = await db.run(postMovieQuery);
+  const movieId = dbResponse.lastID;
+  response.send("Movie Successfully Added");
+});
+
+//API 3 GET
+app.get("/movies/:movieId/", async (request, response) => {
+  const { movieId } = request.params;
+  console.log(movieId);
+  const getMovieQuery = `
+        SELECT 
+          *
+        FROM 
+          movie 
+        WHERE 
+          movie_id = ${movieId};
+     `;
+
+  const movieObject = await db.get(getMovieQuery);
+  response.send(movieObject);
+});
+
+//API 4 PUT
+app.put("/movies/:movieId/", async (request, response) => {
+  const { movieId } = request.params;
+  const requestBody = request.body;
+  const { directorId, movieName, leadActor } = requestBody;
+  console.log(requestBody);
+  const updateMovieQuery = `
+        UPDATE 
+            movie 
+        SET 
+            director_id = ${directorId},
+            movie_name = '${movieName}', 
+            lead_actor = '${leadActor}'
+        WHERE 
+            movie_id = ${movieId};`;
+  await db.run(updateMovieQuery);
+  response.send("Movie Details Updated");
 });

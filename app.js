9a2f9aece30d16b0b265d65378560b0fac9abf3f
Sumeettt <129482701+Sumeettt@users.noreply.github.com>
2023-04-17 11:15:38 +0530

    correct code for all APIs

--- app.js
+++ app.js
@@ -58,7 +58,8 @@ app.post("/movies/", async (request, response) => {
             '${leadActor}'
         );`;
   const dbResponse = await db.run(postMovieQuery);
-  const movieId = dbResponse.lastID;
+  //const movieId = dbResponse.lastID;
+  //console.log(movieId);
   response.send("Movie Successfully Added");
 });
 
@@ -74,9 +75,17 @@ app.get("/movies/:movieId/", async (request, response) => {
         WHERE 
           movie_id = ${movieId};
      `;
-
   const movieObject = await db.get(getMovieQuery);
-  response.send(movieObject);
+  const convertSnackCasetoCamelCase = (snackCase) => {
+    return {
+      movieId: snackCase.movie_id,
+      directorId: snackCase.director_id,
+      movieName: snackCase.movie_name,
+      leadActor: snackCase.lead_actor,
+    };
+  };
+
+  response.send(convertSnackCasetoCamelCase(movieObject));
 });
 
 //API 4 PUT
@@ -84,7 +93,6 @@ app.put("/movies/:movieId/", async (request, response) => {
   const { movieId } = request.params;
   const requestBody = request.body;
   const { directorId, movieName, leadActor } = requestBody;
-  console.log(requestBody);
   const updateMovieQuery = `
         UPDATE 
             movie 
@@ -95,5 +103,72 @@ app.put("/movies/:movieId/", async (request, response) => {
         WHERE 
             movie_id = ${movieId};`;
   await db.run(updateMovieQuery);
+
   response.send("Movie Details Updated");
 });
+
+// API 5 DELETE
+
+app.delete("/movies/:movieId/", async (request, response) => {
+  const { movieId } = request.params;
+  console.log(movieId);
+  const deleteQuery = `
+        DELETE FROM 
+            movie 
+        WHERE 
+            movie_id = ${movieId};
+    `;
+  await db.run(deleteQuery);
+  response.send("Movie Removed");
+});
+
+//API 6 GET
+app.get("/directors/", async (request, response) => {
+  const getDirectorQuery = `
+        SELECT 
+            * 
+        FROM 
+            director;
+    `;
+  const directorList = await db.all(getDirectorQuery);
+
+  const convertSnackCasetoCamelCase = (snackCase) => {
+    return {
+      directorId: snackCase.director_id,
+      directorName: snackCase.director_name,
+    };
+  };
+
+  response.send(
+    directorList.map((eachDirector) => {
+      return convertSnackCasetoCamelCase(eachDirector);
+    })
+  );
+});
+
+//API 7 GET
+app.get("/directors/:directorId/movies/", async (request, response) => {
+  const { directorId } = request.params;
+  const getListOfMoviesDirectedBySpecificQuery = `
+        SELECT
+            movie_name 
+        FROM 
+            movie
+        WHERE 
+            director_id = ${directorId};
+    `;
+
+  const movieList = await db.all(getListOfMoviesDirectedBySpecificQuery);
+
+  const convertSnackCasetoCamelCase = (snackCase) => {
+    return {
+      movieName: snackCase.movie_name,
+    };
+  };
+
+  response.send(
+    movieList.map((eachMovie) => convertSnackCasetoCamelCase(eachMovie))
+  );
+});
+
+module.exports = app;
